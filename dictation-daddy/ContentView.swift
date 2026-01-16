5503c629ea2fecae854811e9ed6ceecfc133e274
rujin2003 <rujindevkota@gmail.com>
2026-01-16 13:46:20 +0530

    first commit

---
+++ dictation-daddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dictation-daddy
+//
+//  Created by Rujin Devkota on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dictation-daddy/dictation_daddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictation_daddyApp.swift
+//  dictation-daddy
+//
+//  Created by Rujin Devkota on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct dictation_daddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
